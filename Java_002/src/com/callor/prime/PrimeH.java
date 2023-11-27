29640f5aa2f60b6f2a13756a634b913009bc95e2
neck091 <cjstk_05@naver.com>
2023-11-28 00:40:58 +0900

    숙제

--- Java_002/src/com/callor/prime/PrimeH.java
+++ Java_002/src/com/callor/prime/PrimeH.java
@@ -1,20 +1,23 @@
 package com.callor.prime;
 
-public class PrimeG {
-
+public class PrimeH {
 	public static void main(String[] args) {
 
-		for (int i = 2; i < 100; i++) {
+		int sum = 0;
+
+		for (int i = 1; i < 100; i++) {
+
 			int index = 0;
 			for (index = 2; index < i + 1; index++) {
 				if ((i + 1) % index == 0) {
 					break;
 				}
 			}
-			if (index >= i + 1) {
-				System.out.println(i + 1 + "는 소수");
-
+			if (index < (i + 1)) {
+			} else {
+				sum += i + 1;
 			}
 		}
+		System.out.println(sum);
 	}
 }
